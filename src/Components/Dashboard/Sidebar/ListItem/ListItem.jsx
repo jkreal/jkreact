c100f68f951b2bf1d75cadb101a2e02391ef1e86
Josh Rowden <jkrowden@gmail.com>
2021-08-20 19:55:04 -0600

    Added a helpful message

--- src/Components/Dashboard/Sidebar/ListItem/ListItem.jsx
+++ src/Components/Dashboard/Sidebar/ListItem/ListItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ListItem = () => {
   return (
     <div className="row listitem">
-      <div className="col-md-12 listtitle"><h2>Stuff</h2></div>
+      <div className="col-md-12 listtitle"><h2>Warning: Do NOT click anything</h2></div>
     </div>
   );
 };
--- src/Components/Dashboard/Sidebar/Sidebar.jsx
+++ src/Components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
-// import ListItem from "./ListItem";
+import ListItem from "./ListItem";
 
 const Sidebar = (props) => {
-    
   return (
     <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 sidebar">
 
-      <div className="col-md-12 sidebar-button-col">
-        <button type="button" className="btn btn-warning sidebar-button" onClick={props.toggleSidebar}> 
-          Collapse
-        </button>
+      <div className="row">
+
+        <div className="col-md-12 sidebar-button-col">
+          <button
+            type="button"
+            className="btn btn-warning sidebar-button"
+            onClick={props.toggleSidebar}
+          >
+            Collapse
+          </button>
+          <ListItem/>
+          <ListItem/>
+        </div>
       </div>
 
       {/* <div className="col-md-12">
