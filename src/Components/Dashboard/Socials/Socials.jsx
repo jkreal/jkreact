0994393b09f832d84acfdfa50b85be57526217bf
Josh <jkrowden@gmail.com>
2021-10-03 16:42:18 -0600

    Suped up the Socials page for desktop viewers

--- src/Components/Dashboard/Socials/Socials.jsx
+++ src/Components/Dashboard/Socials/Socials.jsx
@@ -8,6 +8,7 @@ const aMessage =
   "Why hello there. It's always nice to have visitors. While you're here, why not leave us a message?";
 const aMessage2 =
   "...Oh right, we still need to develop that. Anyway, you can check out our social media pages while you wait!";
+
 class Socials extends React.Component {
   constructor(props) {
     super(props);
@@ -24,6 +25,7 @@ class Socials extends React.Component {
       >
         <Row
           className="social-row"
+          // No minimum height for mobile
           style={this.props.mobile > 1 ? { minHeight: "80vh" } : {}}
         >
           {this.props.sidebar === false ? (
@@ -42,7 +44,10 @@ class Socials extends React.Component {
             <h1 className="content-title">Socials</h1>
           </Col>
 
+          {/* The rest of Socials is rendered depending on user's viewport */}
+
           {this.props.mobile < 2 ? (
+            // This bit is what is rendered when the display is a mobile phone
             <React.Fragment>
               <Row
                 style={{
@@ -55,41 +60,117 @@ class Socials extends React.Component {
                 }}
               >
                 <Col>
-                  <p style={{ fontFamily: "Concert One", marginTop: "1.5em", fontSize: "22px" }}>
+                  <p
+                    style={{
+                      marginTop: "1.5em",
+                      fontSize: "22px",
+                    }}
+                  >
                     {aMessage}
                   </p>
-                  <p style={{ fontFamily: "Concert One", marginTop: "5em", fontSize: "22px" }}>
+                  <p
+                    style={{
+                      marginTop: "5em",
+                      fontSize: "22px",
+                    }}
+                  >
                     {aMessage2}
                   </p>
                 </Col>
               </Row>
               <Row style={{ margin: "auto" }}>
                 <Col className="mobile-social-col">
-                  <a target="_blank" href="https://github.com/jkrowden">
-                    <img src={gitIcon} style={{ width: "100%" }} />
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/jkrowden"
+                  >
+                    <img
+                      alt="notgithub"
+                      src={gitIcon}
+                      style={{ width: "100%" }}
+                    />
                   </a>
                 </Col>
                 <Col className="mobile-social-col">
-                  <a target="_blank" href="https://www.linkedin.com/in/jkrowden/">
-                    <img src={linkedIcon} style={{ width: "100%" }} />
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/jkrowden/"
+                  >
+                    <img
+                      alt="linkedinornot"
+                      src={linkedIcon}
+                      style={{ width: "100%" }}
+                    />
                   </a>
                 </Col>
               </Row>
             </React.Fragment>
           ) : (
+            // This bit is what is rendered for every other viewport
             <Row
               style={{
                 minHeight: "50vh",
-                border: "4px dashed black",
                 margin: "auto",
               }}
             >
-              <Col style={{ border: "4px solid lightgreen" }}>Acol</Col>
-              <Col>
-                <Col className="" style={{ border: "4px solid lightgreen" }}>
-                  Bcol1
+              <Col
+                className="desktop-comment-col"
+                style={{ background: "linear-gradient(whitesmoke, #63a8e6)" }}
+              >
+                <p
+                  style={{
+                    marginTop: "5em",
+                    fontSize: "24px",
+                  }}
+                >
+                  {aMessage}
+                </p>
+                <p
+                  style={{
+                    marginTop: "5em",
+                    fontSize: "24px",
+                  }}
+                >
+                  {aMessage2}
+                </p>
+              </Col>
+              <Col style={{ margin: "auto" }}>
+                <Col
+                  className="desktop-social-col"
+                  style={{
+                    backgroundImage: `url(${gitIcon})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "18vh",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                >
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/jkrowden"
+                  >
+                    <div style={{ height: "20vh" }}></div>
+                  </a>
                 </Col>
-                <Col style={{ border: "4px solid lightgreen" }}>Bcol2</Col>
+                <Col
+                  className="desktop-social-col"
+                  style={{
+                    backgroundImage: `url(${linkedIcon})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "18vh",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                >
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/jkrowden/"
+                  >
+                    <div style={{ height: "20vh" }}></div>
+                  </a>
+                </Col>
               </Col>
             </Row>
           )}
