8867828e7be464f0623042c108b7e6314fd58fac
Josh <jkrowden@gmail.com>
2021-10-03 15:32:59 -0600

    Suped up the Socials page for mobile. Desktop TBD

--- src/Components/Dashboard/Socials/Socials.jsx
+++ src/Components/Dashboard/Socials/Socials.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 import { Row, Col, Button } from "react-bootstrap";
+import gitIcon from "./github-icon.png";
+import linkedIcon from "./linkedin-icon.png";
 
+const aMessage =
+  "Why hello there. It's always nice to have visitors. While you're here, why not leave us a message?";
+const aMessage2 =
+  "...Oh right, we still need to develop that. Anyway, you can check out our social media pages while you wait!";
 class Socials extends React.Component {
   constructor(props) {
     super(props);
@@ -14,16 +20,11 @@ class Socials extends React.Component {
         style={{
           minHeight: "80vh",
           backgroundColor: "green",
-          margin: "auto",
         }}
       >
         <Row
-          style={{
-            borderTop: "4px solid black",
-            borderBottom: "6px solid black",
-            backgroundColor: "#3c6fc2",
-            minHeight: "40vh",
-          }}
+          className="social-row"
+          style={this.props.mobile > 1 ? { minHeight: "80vh" } : {}}
         >
           {this.props.sidebar === false ? (
             <button
@@ -41,41 +42,57 @@ class Socials extends React.Component {
             <h1 className="content-title">Socials</h1>
           </Col>
 
-          <Row
-            style={{
-              minHeight: "20vh",
-              margin: "auto",
-              alignContent: "center",
-              marginBottom: "0",
-              bottom: "0"
-            }}
-          >
-            <Col
-              xs
-              sm={12}
-              md
-              lg={2}
-              xl={2}
-              xxl={3}
-              style={{ border: "3px solid black" }}
+          {this.props.mobile < 2 ? (
+            <React.Fragment>
+              <Row
+                style={{
+                  minHeight: "40vh",
+                  border: "4px solid black",
+                  margin: "auto",
+                  borderBottomRightRadius: "40px",
+                  boxShadow: "4px 0px",
+                  background: "linear-gradient(white, #3c6fc2)",
+                }}
+              >
+                <Col>
+                  <p style={{ fontFamily: "Concert One", marginTop: "1.5em", fontSize: "22px" }}>
+                    {aMessage}
+                  </p>
+                  <p style={{ fontFamily: "Concert One", marginTop: "5em", fontSize: "22px" }}>
+                    {aMessage2}
+                  </p>
+                </Col>
+              </Row>
+              <Row style={{ margin: "auto" }}>
+                <Col className="mobile-social-col">
+                  <a target="_blank" href="https://github.com/jkrowden">
+                    <img src={gitIcon} style={{ width: "100%" }} />
+                  </a>
+                </Col>
+                <Col className="mobile-social-col">
+                  <a target="_blank" href="https://www.linkedin.com/in/jkrowden/">
+                    <img src={linkedIcon} style={{ width: "100%" }} />
+                  </a>
+                </Col>
+              </Row>
+            </React.Fragment>
+          ) : (
+            <Row
+              style={{
+                minHeight: "50vh",
+                border: "4px dashed black",
+                margin: "auto",
+              }}
             >
-              Blank
-            </Col>
-            <Col style={{ border: "3px solid black" }}>One Social</Col>
-            <Col style={{ border: "3px solid black" }}>Two Social</Col>
-            <Col style={{ border: "3px solid black" }}>Three socials</Col>
-            <Col
-              xs
-              sm={12}
-              md
-              lg={2}
-              xl={2}
-              xxl={3}
-              style={{ border: "3px solid black" }}
-            >
-              Blank
-            </Col>
-          </Row>
+              <Col style={{ border: "4px solid lightgreen" }}>Acol</Col>
+              <Col>
+                <Col className="" style={{ border: "4px solid lightgreen" }}>
+                  Bcol1
+                </Col>
+                <Col style={{ border: "4px solid lightgreen" }}>Bcol2</Col>
+              </Col>
+            </Row>
+          )}
         </Row>
       </Col>
     );
